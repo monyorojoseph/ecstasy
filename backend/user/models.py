a0cc23aa105492ec27a3d90ea347d46d7826fc23
monyorojoseph <monyorojoseph@gmail.com>
2022-08-25 12:44:40 +0300

    Commit 4

---
+++ backend/user/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError("Please provide an email")
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(email,
+                username=username, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=50)
+    username = models.CharField(unique=True, max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_worker = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin    
+            
